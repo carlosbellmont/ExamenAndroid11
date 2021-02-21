7ef4538d2dce06cb074a8959b2e028d13f4f8d30
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-02-22 00:11:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/esqueletoejercicioapi/AdapterString.kt
@@ -0,0 +1,44 @@
+package com.example.esqueletoejercicioapi
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class AdapterString(private var datos : List<String>) : RecyclerView.Adapter<AdapterString.StringViewHolder>()  {
+
+
+    class StringViewHolder(val root: View, val textView: TextView) : RecyclerView.ViewHolder(root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main_item, parent, false)
+        val textView = view.findViewById<TextView>(R.id.tv_activity_main_item)
+        return StringViewHolder(view, textView)
+    }
+
+    override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
+        holder.textView.text = datos[position]
+        if (position%2 ==0){
+            holder.root.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.design_default_color_primary_variant))
+        } else {
+            holder.root.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.design_default_color_primary_variant))
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
+    }
+
+    suspend fun setData(string : List<String>){
+        datos = string
+        withContext(Dispatchers.Main){
+            notifyDataSetChanged()
+        }
+    }
+
+}
